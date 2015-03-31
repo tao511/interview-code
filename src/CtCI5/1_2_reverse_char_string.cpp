430bca10ced03f96dc5e3c3c305fee2b284a5399
bxshi <baoxu.shi@gmail.com>
2015-03-31 11:25:03 -0400

    add comments

--- src/CtCI5/1_2_reverse_char_string.cpp
+++ src/CtCI5/1_2_reverse_char_string.cpp
@@ -1,6 +1,19 @@
 #ifndef SOLUTION_REVERSE_CHAR_STRING
 #define SOLUTION_REVERSE_CHAR_STRING
 
+/**
+ * Reverse a \0 terminated char* string.
+ *
+ * Keep two pointers, one at the beginning and the other one 
+ * at the end of this char* string. Swap the values that these 
+ * two pointers points too, and then move them to the middle by 1.
+ * 
+ * Reminder do not reverse the endding \0.
+ *
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+ *
+ */
 void reverse(char* str) {
 
 	size_t end = 0;
