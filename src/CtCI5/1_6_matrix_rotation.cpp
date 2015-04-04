62b19d26a52078fb38a66cb684b56d622a725e78
bxshi <baoxu.shi@gmail.com>
2015-04-04 17:55:02 -0400

    add examples

--- src/CtCI5/1_6_matrix_rotation.cpp
+++ src/CtCI5/1_6_matrix_rotation.cpp
@@ -1,7 +1,9 @@
 #ifndef INTERVIEW_MATRIX_ROTATION
 #define INTERVIEW_MATRIX_ROTATION
 
-/** Rotate 90 degree does NOT equals to transpose
+/** 
+ * Rotate 90 degree does NOT equals to transpose.
+ * 
  * An example is
  *  1, 2, 3, 4;
  *  5, 6, 7, 8;
@@ -20,7 +22,37 @@
  *  15,11, 7, 3
  *  16,12, 8, 4
  *
- *  The rotation happens in the following way:
+ *  First lets mark the matrtix by its index
+ *
+ *  0,0 0,1 0,2 0,3
+ *  1,0 1,1 1,2 1,3
+ *  2,0 2,1 2,2 2,3
+ *	3,0 3,1 3,2 3,3
+ *
+ *	The rotation of 0,0 is
+ *	0,0 -> 0,3
+ *	0,3 -> 3,3
+ *	3,3 -> 3,0
+ *	3,0 -> 0,0
+ *
+ *	And the rotation of 0,1 is
+ *	0,1 -> 1,3
+ *	1,3 -> 3,2
+ *	3,2 -> 2,0
+ *	2,0 -> 0,1
+ *
+ *  As we can see, row 0 becomes column n, column n becomes
+ *  row n, row n converts to column 0, and column now is row 0.
+ *
+ *  Therefore, the rotation of row 0,n column 0, is:
+ *
+ *  (0,i) -> (i,n)
+ *  (i,n) -> (n, n-i)
+ *  (n, n-i) -> (n-i, 0)
+ *  (n-i, 0) -> (0,i)
+ *  
+ *  And for row i, n-i, colum i, n-i, the definition of transform is:
+ *  
  *  (i,j) => (j, n-i) => (n-i,n-j) => (n-j,i) => (i,j)
  *
  *  REMEMBER, `n` here is the maximum index, NOT THE SIZE
